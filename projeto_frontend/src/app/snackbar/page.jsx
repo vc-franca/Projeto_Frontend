36c3f4061c0b92b3236255bf5f1275b31053c330
Yanpemn <victorcestafran08@gmail.com>
2025-03-28 17:17:05 -0300

    primeiro commit

---
+++ projeto_frontend/src/app/snackbar/page.jsx
@@ -0,0 +1,53 @@
+export default function SnackBar() {
+
+    const snacks = [
+        {
+            id: 1,
+            nome: 'pipoca',
+            preco: 25.50,
+            desc: '',
+            img: '',
+        },
+        {
+            id: 2,
+            nome: 'pipoca',
+            preco: 25.50,
+            desc: '',
+            img: '',
+        },
+        {
+            id: 3,
+            nome: 'pipoca',
+            preco: 25.50,
+            desc: '',
+            img: '',
+        },
+        {
+            id: 4,
+            nome: 'pipoca',
+            preco: 25.50,
+            desc: '',
+            img: '',
+        },
+        {
+            id: 5,
+            nome: 'pipoca',
+            preco: 25.50,
+            desc: '',
+            img: '',
+        },
+        {
+            id: 6,
+            nome: 'pipoca',
+            preco: 25.50,
+            desc: '',
+            img: '',
+        },
+    ];
+
+    return(
+        <section className="">
+            <div></div>
+        </section>
+    );
+}
---
+++ projeto_frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return(
+        <footer className="d-flex justify-content-center"></footer>
+    );
+}
