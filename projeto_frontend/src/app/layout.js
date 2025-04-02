bc05ef861a843cc58e3834a0adf4b2bd88a73dc6
Yanpemn <victorcestafran08@gmail.com>
2025-04-02 17:05:16 -0300

    row de snacks feita

--- projeto_frontend/src/app/layout.js
+++ projeto_frontend/src/app/layout.js
@@ -18,11 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`} style={{ minHeight: "100vh" }}>
         {children}
       </body>
     </html>
--- projeto_frontend/src/app/snackbar/page.jsx
+++ projeto_frontend/src/app/snackbar/page.jsx
@@ -1,53 +1,81 @@
-export default function SnackBar() {
+export default function Snackbar() {
 
     const snacks = [
         {
             id: 1,
             nome: 'pipoca',
             preco: 25.50,
-            desc: '',
-            img: '',
+            desc: 'Pipoca cremosa com coberturas opcionais',
+            img: '/pipoca.jfif',
         },
         {
             id: 2,
             nome: 'pipoca',
             preco: 25.50,
-            desc: '',
-            img: '',
+            desc: 'Pipoca cremosa com coberturas opcionais',
+            img: '/pipoca.jfif',
         },
         {
             id: 3,
             nome: 'pipoca',
             preco: 25.50,
-            desc: '',
-            img: '',
+            desc: 'Pipoca cremosa com coberturas opcionais',
+            img: '/pipoca.jfif',
         },
         {
             id: 4,
             nome: 'pipoca',
             preco: 25.50,
-            desc: '',
-            img: '',
+            desc: 'Pipoca cremosa com coberturas opcionais',
+            img: '/pipoca.jfif',
         },
         {
             id: 5,
             nome: 'pipoca',
             preco: 25.50,
-            desc: '',
-            img: '',
+            desc: 'Pipoca cremosa com coberturas opcionais',
+            img: '/pipoca.jfif',
         },
         {
             id: 6,
             nome: 'pipoca',
             preco: 25.50,
-            desc: '',
-            img: '',
+            desc: 'Pipoca cremosa com coberturas opcionais',
+            img: '/pipoca.jfif',
         },
     ];
 
-    return(
-        <section className="">
-            <div></div>
-        </section>
+    const combos = [
+        {
+            id: 1,
+            nome: 'pipoca',
+            preco: 25.50,
+            desc: 'Pipoca cremosa com coberturas opcionais',
+            img: '/pipoca.jfif',
+        },
+    ];
+
+    return (
+        <div className="bg-black text-white" style={{ minHeight: "100vh" }}>
+            <section>
+                <h2>Combos Especiais</h2>
+
+                <div>
+
+                </div>
+            </section>
+            <section>
+                <h2>Ache seu snack ideal</h2>
+                <div className="row mr-3 d-flex justify-content-center" style={{ marginLeft: "2%", gap: "0.3" }}>
+                    {snacks.map((snack) => (
+                        <div key={snack.id} className="col w-180">
+                            <img src={snack.img} alt={snack.name} className="rounded-4" style={{ width: "165px" }} />
+                            <p>{snack.desc}</p>
+                            <div>{snack.name}</div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </div>
     );
 }
