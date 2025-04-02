752754b27afbe6902533a72792fb7ebe265a24cd
rossato08 <gabriel.rossato050208@gmail.com>
2025-04-02 17:09:35 -0300

    regreg
    ergerg

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import Header from "../components/Header/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +29,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
 
-        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
       </body>
     </html>
   );
